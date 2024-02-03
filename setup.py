6cec893414c9cfb8056fa195d736937b312a28e5
nickdecodes <ncickdecodes@163.com>
2024-02-03 19:26:40 +0800

    add read the docs

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='lottokit',
     version='0.2',
@@ -22,7 +25,7 @@ setup(
     author="nickdecodes",
     author_email="nickdecodes@163.com",
     description="Lotto Kit Package",
-    long_description='readme',
+    long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.6",
     install_requires=[
